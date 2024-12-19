9ddeab71661fe6f5b5c58c10f9b101accd2efd90
K <kira.kisel2008@gmail.com>
2024-12-19 23:57:17 +0300

    Update main.py

--- collision/module.py
+++ collision/module.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 class RectCorrectError(Exception): #исключение(ошибка) при неправильном вводе координат
     def __init__(self, num):
         super().__init__(f"{num}й прямоугольник некоректный")
@@ -18,3 +20,30 @@ def intersectionAreaRect(fir_rect, sec_rect):
         raise RectCorrectError(1)
     elif (not isCorrectRect(sec_rect)): #если 2 прямоугольник некорректный- ошибка
         raise RectCorrectError(2)
+
+def intersectionAreaMultiRect(rectangles):
+
+    def get_intersection(rects):
+        x1 = max(rect[0][0] for rect in rects)
+        y1 = max(rect[0][1] for rect in rects)
+        x2 = min(rect[1][0] for rect in rects)
+        y2 = min(rect[1][1] for rect in rects)
+        if x1 < x2 and y1 < y2:
+            return [(x1, y1), (x2, y2)]
+        return None
+    
+    total_area = 0 # уникальная площадь
+    all_intersections = [] # массив всех пересечений
+
+    
+    for combination in combinations(rectangles, 2):# суммарная площадь всех пересечений двух фигур
+            intersection = get_intersection(combination)
+            if intersection:
+                all_intersections.append(intersection)
+    
+    for k in range(1, len(all_intersections) + 1):# итоговая площадь пересечений двух и более фигур
+        sign = (-1) ** (k + 1)  # Чередование знаков
+        for combination in combinations(all_intersections, k):
+            intersection = get_intersection(combination)
+            total_area += sign * intersectionAreaRect(intersection)
+    return total_area
